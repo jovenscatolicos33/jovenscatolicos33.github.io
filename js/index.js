0bfb4a536dc35b1e14967b8ecba3b9cf99433d7e
Layon Martins Fonseca <layonmartins03@gmail.com>
2015-12-22 17:51:57 -0200

    update loading page with .load();

--- js/index.js
+++ js/index.js
@@ -1,7 +1,8 @@
 $(document).ready(function () {
-	
+	$(window).load(function() {
 	$("#body").fadeIn();
 	$("#loading").fadeOut();
+	});
 	$(".contacts img").mouseover(function() {playsong();});
 });
 
