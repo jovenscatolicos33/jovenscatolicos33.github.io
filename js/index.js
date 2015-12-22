1443ecd57a1d97214686389076a88c3d1f8ac25d
Layon Martins Fonseca <layonmartins03@gmail.com>
2015-12-22 21:09:58 -0200

    update background flowers

--- js/index.js
+++ js/index.js
@@ -1,11 +1,19 @@
 $(document).ready(function () {
+	
+	//screen the loading
 	$(window).load(function() {
 	$("#body").fadeIn();
 	$("#loading").fadeOut();
 	});
+	
+	//play song when mouseover img contacts
 	$(".contacts img").mouseover(function() {playsong();});
+
+	//active wow.js 'scroll with animate.css'
+	new WOW().init();
 });
 
+//function for play song
 function playsong(){
 	document.getElementById("sound").play();
 }
