a844fc27ad48c70dc3ae24c4e733340959e82f76
Layon Martins Fonseca <layonmartins03@gmail.com>
2015-12-22 23:32:06 -0200

    add function scroll automatic

--- js/index.js
+++ js/index.js
@@ -4,13 +4,30 @@ $(document).ready(function () {
 	$(window).load(function() {
 	$(".sections").fadeIn();
 	$("#loading").fadeOut();
+
 	//active wow.js 'scroll with animate.css'
 	new WOW().init();
+
+	//controll scroll
+	$("#up").click(function() {$('html, body').animate({scrollTop:0}, 'slow');});
+	$("#down").click(function() {$('html, body').animate({scrollTop:3500}, 'slow');});
 	});
-	
+
 	//play song when mouseover img contacts
 	$(".contacts img").mouseover(function() {playsong();});
 
+	//controll show scroll
+	window.onscroll = function () {
+		if (window.scrollY < 100)
+			$("#up").fadeOut();
+		else
+			$("#up").fadeIn();
+
+		if (window.scrollY > 3100)
+			$("#down").fadeOut();
+		else
+			$("#down").fadeIn();
+	}
 	
 });
 
